44f7f4b096e6b45b46a9114651c51c362a9ad5b9
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-05-12 10:38:24 +0300

    Create handle_exit.c

---
+++ handle_exit.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+/**
+ * handle_exit - This function handles the "exit" command in a shell program
+ * allowing the user to exit with an optional exit status
+ * @args: Pointer to array of strings with command arguments.
+ * @num_args: Number of arguments, including command
+ **/
+void handle_exit(char **args, int num_args)
+{
+	int exit_status;
+
+	if (num_args == 1)
+	{
+		exit(0);
+	}
+	else if (num_args == 2)
+	{
+		exit_status = atoi(args[0]);
+		if (exit_status < 0)
+		{
+			fprintf(stderr, "./hsh: 1:exit: Illegal number: %d", atoi(args[1]));
+			exit(0);
+		}
+
+		exit(exit_status);
+	}
+	else
+	{
+		printf("Usage: exit status\n");
+	}
+}
