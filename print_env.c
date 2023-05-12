4547dec65ee34ffbb1a169136b5cb35cc233aac8
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-05-12 10:47:56 +0300

    Create print_env.c

---
+++ print_env.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * print_environment - prints all environment variables and their values
+ * using environ, strtok(), and getenv()
+ * Return: void
+ **/
+/**/
+void print_environment(void)
+{
+	char **envp = __environ;
+
+	while (*envp != NULL)
+	{
+		char *name = strchr(*envp, '=');
+
+		if (name != NULL)
+		{
+			*name = '\0';
+			name++;
+			printf("%s=%s\n", *envp, name);
+		}
+		envp++;
+	}
+}
+/*
+ * void print_environment(void)
+{
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+}
+*/
