31f98567fe64da5857e911949dc8ac17af737bf2
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-05-24 18:06:39 +0300

    Update handle_separators.c

--- handle_separators.c
+++ handle_separators.c
@@ -70,49 +70,8 @@ void handle_command_line_separators2(char *command)
  * Return: void
  */
 
+
 void handle_command_line_separators(char *command)
-{
-	const char *separator = ";";
-	char *token, *trimmed_command;
-	pid_t pid;
-	int num_args = 0;
-	char *args[64];
-	int exit_status = 0;
-
-	token = strtok(command, separator);
-	while (token != NULL)
-	{
-		trimmed_command = trim(token);
-		pid = fork();
-		if (pid == 0)
-		{
-			args[num_args++] = strtok(trimmed_command, " ");
-			while ((args[num_args++] = strtok(NULL, " ")) != NULL)
-				;
-			execvp(args[0], args);
-			perror("execvp");
-			exit(1);
-		}
-		else if (pid > 0)
-		{
-			int status;
-
-			waitpid(pid, &status, 0);
-			if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-			{
-				exit_status = 2;
-			}
-		}
-		else
-		{
-			perror("fork");
-			exit(1);
-		}
-		token = strtok(NULL, separator);
-	}
-	exit(exit_status);
-}
-void handle_command_line_separators3(char *command)
 {
 	const char *delimiters = "&|;";
 	char *token, *trimmed_command;
