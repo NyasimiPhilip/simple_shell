50aebbd8b81e26c8846b482b78b7f7f7739e9a28
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-05-24 12:45:12 +0300

    Update handle_separators.c

--- handle_separators.c
+++ handle_separators.c
@@ -28,58 +28,42 @@ char *trim(char *str)
  */
 void handle_command_line_separators2(char *command)
 {
-    const char *delimiters = "||&&";
-    char *token, *trimmed_command;
-    int exit_status = 0;
-    char *args[64];
+	const char *delimiters = "||&&";
+	char *token;
+	int exit_status = 0;
+	char *args[64];
 
-    while ((token = strtok(command, delimiters)) != NULL)
-    {
-        trimmed_command = trim(token);
-        pid_t pid = fork();
-        if (pid == 0)
-        {
-            int num_args = 0;
-            args[num_args++] = strtok(trimmed_command, " ");
-            while ((args[num_args++] = strtok(NULL, " ")) != NULL);
+	while ((token = strtok(command, delimiters)) != NULL)
+	{
+		char *trimmed_command = trim(token);
+		pid_t pid = fork();
 
-            execvp(args[0], args);
-            perror("execvp");
-            exit(1);
-        }
-        else if (pid > 0)
-        {
-            int status;
-            waitpid(pid, &status, 0);
-            int child_exit_status = WEXITSTATUS(status);
+		if (pid == 0)
+		{
+			int num_args = 0;
 
-            if (exit_status == 0 && token[0] == '|')
-            {
-                if (child_exit_status == 0)
-                    break;
-            }
-            else if (exit_status != 0 && token[0] == '&')
-            {
-                if (child_exit_status != 0)
-                    break;
-            }
+			args[num_args++] = strtok(trimmed_command, " ");
+			while
+				((args[num_args++] = strtok(NULL, " ")) != NULL);
+			execvp(args[0], args);
+			perror("execvp");
+			exit(1);
+		}
+		else if (pid > 0)
+		{
+			int status;
+			int child_exit_status = WEXITSTATUS(status);
 
-            if (exit_status == 0 || child_exit_status != 0)
-                exit_status = child_exit_status;
-        }
-        else
-        {
-            perror("fork");
-            exit(1);
-        }
-
-        command = NULL; // Reset strtok for subsequent tokens
-    }
-
-    if (exit_status != 0)
-        printf("Command failed with exit status %d\n", exit_status);
+			waitpid(pid, &status, 0);
+			if ((exit_status == 0 && token[0] == '|') && child_exit_status == 0)
+				break;
+			else if ((exit_status != 0 && token[0] == '&') && child_exit_status != 0)
+				break;
+			if (exit_status == 0 || child_exit_status != 0)
+				exit_status = child_exit_status;
+		}
+	}
 }
-
 /**
  * handle_command_line_separators - handles command line separators
  * @command: command passed
@@ -95,7 +79,7 @@ void handle_command_line_separators(char *command)
 	char *args[64];
 	int exit_status = 0;
 
-	tioken = strtok(command, separator);
+	token = strtok(command, separator);
 	while (token != NULL)
 	{
 		trimmed_command = trim(token);
